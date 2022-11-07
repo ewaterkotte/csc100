552d61edfedf6aafbb783180abc7ef569b5a000d
ewaterkotte <22508345+ewaterkotte@users.noreply.github.com>
2022-11-07 11:47:39 -0800

    Create Mod7-Assignment2-OptionalChallenge.java
    Copy of the Module 7 challenge starter code.

---
+++ Mod7-Assignment2-OptionalChallenge.java
@@ -0,0 +1,54 @@
+class Main {
+  public static void main(String[] args) {
+    
+    //------------------------------------------------------
+    //Counting from 0 - 9 by ones
+    System.out.println("Counting from 0 - 9 by ones");
+    //Count from 0 - 9
+    for (int i=0; i<10; i++)
+    {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    //------------------------------------------------------
+
+    //------------------------------------------------------
+    //Counting from 0 - 10 by twos
+    System.out.println("Counting from 0 - 10 by twos");
+    //Count from 0 - 10 by twos
+    for (int i=0; i<=10; i+=2)
+    {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    //------------------------------------------------------
+
+    //------------------------------------------------------
+    //Counting 10 - 0 backwards
+    System.out.println("Counting 10 - 0 backwards");
+    //Count from 0 - 9
+    for (int i=10; i>=0; i--)
+    {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    //------------------------------------------------------
+
+    //------------------------------------------------------
+    //TODO #1: Create a loop that counts from 1 - 25 forwards
+    //------------------------------------------------------
+
+    //------------------------------------------------------
+    //TODO #2: Create a loop that counts from 0 - 21 by 3s
+    //------------------------------------------------------
+
+    //------------------------------------------------------
+    //TODO #3: Create a loop that counts backwards from 18 - 0
+    //------------------------------------------------------
+
+    //------------------------------------------------------
+    //EXTREME CHALLANEGE: Create a loop that goes from 0 - 30 
+    // print ONLY the even numbers between 0 and 30. [hint: use %]
+    //------------------------------------------------------
+  }
+}
